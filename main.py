7767161e7c0d7a37ae1f52d16aa023483081c1ef
alexpariy1321 <alex.pariy1321@gmail.com>
2025-11-26 15:48:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# ВСТАВЬТЕ СЮДА ВАШ ТОКЕН ВНУТРИ КАВЫЧЕК
+TOKEN = "8429387047:AAGDsLN7rTTZJQNr52FhDHcLxP-3-NAUQKY"
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=TOKEN)
+# Диспетчер (это мозг, который распределяет сообщения)
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="👋 Обо мне")],
+        [types.KeyboardButton(text="💼 Мои услуги")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await message.answer("Привет! Я ваш HR-помощник. Чем могу помочь?", reply_markup=keyboard)
+
+# Хэндлер на нажатие кнопок
+@dp.message()
+async def echo_handler(message: types.Message):
+    if message.text == "👋 Обо мне":
+        await message.answer("Я HR-консультант с опытом...")
+    elif message.text == "💼 Мои услуги":
+        await message.answer("1. Консультация\n2. Разбор резюме")
+    else:
+        await message.answer("Я пока понимаю только кнопки!")
+
+# Запуск процесса
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
